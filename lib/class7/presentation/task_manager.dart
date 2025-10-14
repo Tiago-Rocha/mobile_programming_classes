7b0059ba268bde05bb5f41390770ac542bff23f0
MiguelFelgueiras <miguelfonsecafelgueiras@gmail.com>
2025-10-14 15:44:18 +0000

    refactor(user_manager): Magic number in the register function

--- lib/class7/presentation/task_manager.dart
+++ lib/class7/presentation/task_manager.dart
@@ -35,7 +35,10 @@ class TaskManager extends ChangeNotifier {
   Future<void> deleteTask(String taskId) async {
     // TODO
     // 1. Delete from repository
+    await repository.delete(taskId);
     // 2. Reload tasks
+    await _loadTasks();
     // 3. Notify listeners
+    notifyListeners();
   }
 }
