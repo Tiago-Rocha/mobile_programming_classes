d16e0a28d0d9db40a50ee4efa839e2889db52d41
felgueiras102 <alexandrefonsecafelgueiras@gmail.com>
2025-10-16 14:43:57 +0000

    fix(typo):import fix (#29)

--- lib/class6/+shared_preferences.dart
+++ lib/class6/+shared_preferences.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../class6/main.js';
+import '../class6/main.dart';
 
 extension TaskSharedPreference on SharedPreferences {
   Future<bool> storeTask(Task task) async {
